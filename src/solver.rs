cbd99b60d1316426b641ccb625b2711bb11b9e71
Martin Taibr <taibr.martin@gmail.com>
2017-09-30 07:54:29 +0200

    solver

---
+++ src/solver.rs
@@ -0,0 +1,303 @@
+use std::collections::{BinaryHeap, HashMap, HashSet};
+
+use data::*;
+
+const UP: Dir = Dir { r: -1, c: 0 };
+const RIGHT: Dir = Dir { r: 0, c: 1 };
+const DOWN: Dir = Dir { r: 1, c: 0 };
+const LEFT: Dir = Dir { r: 0, c: -1 };
+const DIRECTIONS: [Dir; 4] = [UP, RIGHT, DOWN, LEFT];
+
+fn expand(map: &MapState, state: &State) -> Vec<State> {
+    //expand_move(map, state)
+    expand_push(map, state)
+}
+
+fn heuristic(map: &MapState, state: &State) -> i32 {
+    //heuristic_move(map, state)
+    heuristic_push(map, state)
+}
+
+pub fn mark_dead_ends(map: &mut MapState) {
+    // init first since otherwise we would use this partially initialized in search()
+    for r in 0..map.map.len() {
+        map.dead_ends.push(Vec::new());
+        for _ in &map.map[r] {
+            map.dead_ends[r].push(false);
+        }
+    }
+
+    for r in 0..map.map.len() {
+        'cell: for c in 0..map.map[r].len() {
+            let box_pos = Pos { r: r as i32, c: c as i32 };
+            if let &Cell::Wall = map.at(box_pos) {
+                //print!("w");
+                continue;
+            }
+
+            for dir in DIRECTIONS.iter() {
+                let player_pos = box_pos + *dir;
+                if let &Cell::Wall = map.at(player_pos) { continue; }
+
+                let fake_state = State {
+                    player_pos: player_pos,
+                    boxes: vec![box_pos],
+                };
+                if let Some(_) = search(map, &fake_state, false) {
+                    //print!("cont");
+                    continue 'cell; // need to find only one solution
+                }
+            }
+            //print!("true");
+            map.dead_ends[r][c] = true; // no solution from any direction
+            //print!("|");
+        }
+        //println!();
+    }
+    //println!();
+
+    for r in 0..map.map.len() {
+        for c in 0..map.map[r].len() {
+            if map.dead_ends[r][c] {
+                print!("{}", 1);
+            } else {
+                print!("{}", 0);
+            }
+        }
+        println!();
+    }
+}
+
+fn heuristic_push(map: &MapState, state: &State) -> i32 {
+    let mut goal_dist_sum = 0;
+    for box_pos in &state.boxes {
+        let mut min = i32::max_value();
+        for goal in &map.goals {
+            let dist = box_pos.dist(*goal);
+            if dist < min {
+                min = dist;
+            }
+        }
+        goal_dist_sum += min;
+    }
+    goal_dist_sum
+}
+
+#[allow(unused)]
+fn heuristic_move(map: &MapState, state: &State) -> i32 {
+    // less is better
+
+    let mut closest_box = i32::max_value();
+    for box_pos in &state.boxes {
+        let dist = state.player_pos.dist(*box_pos);
+        if dist < closest_box {
+            closest_box = dist;
+        }
+    }
+
+    let mut goal_dist_sum = 0;
+    for box_pos in &state.boxes {
+        let mut min = i32::max_value();
+        for goal in &map.goals {
+            let dist = box_pos.dist(*goal);
+            if dist < min {
+                min = dist;
+            }
+        }
+        goal_dist_sum += min;
+    }
+
+    closest_box + goal_dist_sum
+}
+
+pub fn search(map: &MapState, initial_state: &State, print_status: bool) -> Option<Vec<State>> {
+    let mut expands = 0;
+    let mut state_counts = Vec::new();
+    state_counts.push(0);
+
+    let mut to_visit = BinaryHeap::new();
+    let mut closed = HashSet::new();
+    let mut prev = HashMap::new();
+
+    let h = heuristic(&map, &initial_state);
+    let start = SearchState {
+        state: initial_state.clone(),
+        prev: None,
+        dist: 0,
+        h: h,
+    };
+    to_visit.push(start);
+    while let Some(current) = to_visit.pop() {
+        //println!("Trying (dist {}):\n{}", current.dist, map.clone().with_state(&current.state).to_string());
+
+        if closed.contains(&current.state) { continue; }
+
+        if current.dist > (state_counts.len() - 1) as i32 {
+            state_counts.push(0);
+            if print_status {
+                println!("Depth: {}", current.dist);
+                /*if current.dist == 50 {
+                    for ss in &to_visit {
+                        println!("{}", map.clone().with_state(&ss.state).to_string());
+                    }
+                }*/
+            }
+        }
+
+        state_counts[current.dist as usize] += 1;
+
+        // insert here and not as soon as we discover it
+        // otherwise we overwrite the shortest path with longer ones
+        if let Some(p) = current.prev {
+            prev.insert(current.state.clone(), p.clone());
+        }
+
+        if solved(map, &current.state) {
+            if print_status {
+                println!("Expands: {}", expands);
+                println!("Visited states in depth:");
+                for i in 0..state_counts.len() {
+                    println!("{}: {}", i, state_counts[i]);
+                }
+            }
+
+            return Some(backtrack_path(&prev, &current.state));
+        }
+
+        expands += 1;
+        for neighbor_state in expand(&map, &current.state) {
+            // TODO this could probably be optimized a bit by allocating on the heap
+            // and storing references only (to current state, neighbor state is always different)
+            //prev.insert(neighbor_state.clone(), current.state.clone());
+
+            // rust's binary heap doesn't support update_key() so we always insert and then ignore duplicates
+            let h = heuristic(&map, &neighbor_state);
+            let next = SearchState {
+                state: neighbor_state,
+                prev: Some(current.state.clone()),
+                dist: current.dist + 1,
+                h: h,
+            };
+            to_visit.push(next);
+        }
+
+        closed.insert(current.state);
+    }
+
+    if print_status {
+        println!("Expands: {}", expands);
+    }
+
+    None
+}
+
+fn backtrack_path(prev: &HashMap<State, State>, final_state: &State) -> Vec<State> {
+    let mut ret = Vec::new();
+    let mut state = final_state;
+    loop {
+        ret.push(state.clone());
+        if let Some(prev) = prev.get(state) {
+            state = prev;
+        } else {
+            ret.reverse();
+            return ret;
+        }
+    }
+}
+
+fn solved(map: &MapState, state: &State) -> bool {
+    // to detect dead ends, this has to test all boxes are on a goal, not that all goals have a box
+    for pos in &state.boxes {
+        if let &Cell::Path(_, Tile::Goal) = map.at(*pos) {} else {
+            return false;
+        }
+    }
+    true
+}
+
+fn expand_push(map: &MapState, state: &State) -> Vec<State> {
+    let mut new_states = Vec::new();
+
+    let map_state = map.clone().with_boxes(&state);
+
+    let mut reachable = Vec::new();
+    for r in 0..map.map.len() {
+        reachable.push(Vec::new());
+        for _ in 0..map.map[r].len() {
+            reachable[r].push(false)
+        }
+    }
+
+    mark_reachable(&map_state, &mut reachable, state.player_pos, state, &mut new_states);
+
+    new_states
+}
+
+fn mark_reachable(map_state: &MapState, reachable: &mut Vec<Vec<bool>>,
+                  pos: Pos, state: &State, new_states: &mut Vec<State>) {
+    let r = pos.r as usize;
+    let c = pos.c as usize;
+    reachable[r][c] = true;
+    for dir in DIRECTIONS.iter() {
+        let new_pos = pos + *dir;
+        if let Cell::Path(Content::Empty, _) = *map_state.at(new_pos) {
+            if !reachable[new_pos.r as usize][new_pos.c as usize] {
+                mark_reachable(map_state, reachable, new_pos, state, new_states);
+            }
+        } else if let Cell::Path(Content::Box, _) = *map_state.at(new_pos) {
+            let behind_box = new_pos + *dir;
+            if let Cell::Path(Content::Empty, _) = *map_state.at(behind_box) {
+                if !map_state.dead_ends[behind_box.r as usize][behind_box.c as usize] {
+                    let mut new_boxes = state.boxes.clone();
+                    for box_pos in &mut new_boxes {
+                        if *box_pos == new_pos {
+                            *box_pos = behind_box;
+                        }
+                    }
+                    let new_state = State {
+                        player_pos: new_pos,
+                        boxes: new_boxes,
+                    };
+                    new_states.push(new_state);
+                }
+            }
+        }
+    }
+}
+
+#[allow(unused)]
+fn expand_move(map: &MapState, state: &State) -> Vec<State> {
+    let mut new_states = Vec::new();
+
+    let map_state = map.clone().with_boxes(&state);
+    for dir in DIRECTIONS.iter() {
+        let new_pos = state.player_pos + *dir;
+        if let Cell::Path(Content::Empty, _) = *map_state.at(new_pos) {
+            let new_state = State {
+                player_pos: new_pos,
+                boxes: state.boxes.clone(),
+            };
+            new_states.push(new_state);
+        } else if let Cell::Path(Content::Box, _) = *map_state.at(new_pos) {
+            let behind_box = new_pos + *dir;
+            if let Cell::Path(Content::Empty, _) = *map_state.at(behind_box) {
+                if !map.dead_ends[behind_box.r as usize][behind_box.c as usize] {
+                    // goal will never be a dead end - no need to check
+                    let mut new_boxes = state.boxes.clone();
+                    for box_pos in &mut new_boxes {
+                        if *box_pos == new_pos {
+                            *box_pos = behind_box;
+                        }
+                    }
+                    let new_state = State {
+                        player_pos: new_pos,
+                        boxes: new_boxes,
+                    };
+                    new_states.push(new_state);
+                }
+            }
+        }
+    }
+
+    new_states
+}
