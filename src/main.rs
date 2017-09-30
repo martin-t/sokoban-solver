90691a424956473d7bec889b9d3bece02ecee1f0
Martin Taibr <taibr.martin@gmail.com>
2017-09-30 12:10:11 +0200

    clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate clap;
+
 mod formatter;
 mod solver;
 mod data;
@@ -6,13 +8,28 @@ mod utils;
 use std::env;
 use std::process;
 
+use clap::{App, Arg, ArgGroup};
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        println!("Usage: sokoban-solver <path_to_puzzle>");
-        process::exit(1);
-    }
-    let path = &args[1];
+    let matches = App::new("sokoban-solver")
+        .author("martin-t")
+        .version("0.0")
+        .arg(Arg::with_name("custom")
+            .short("-c")
+            .long("--custom")
+            .help("parse as custom format"))
+        .arg(Arg::with_name("xsb")
+            .short("-x")
+            .long("--xsb")
+            .help("parse as XSB format"))
+        .group(ArgGroup::with_name("format")
+            .arg("custom")
+            .arg("xsb")
+            .required(true))
+        .arg(Arg::with_name("file"))
+        .get_matches();
+
+    let path = matches.value_of("file").unwrap();
 
     let puzzle = utils::load_file(path).unwrap_or_else(|err| {
         let current_dir = env::current_dir().unwrap();
