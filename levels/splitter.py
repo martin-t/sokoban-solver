2e5c31906ab72bbe16b105a1c51a3e74148df641
Martin Taibr <taibr.martin@gmail.com>
2017-10-07 15:55:00 +0200

    split boxxle2 and original

--- levels/splitter.py
+++ levels/splitter.py
@@ -1,10 +1,23 @@
-f = open("Boxxle1.txt", 'r')
-s = f.read()
-p = ';.*\n\n'
-re.split(p,s)
-levels = re.split(p,s)
-for i in range(len(levels)):
-	of = open("boxxle1/{}".format(i+1), 'w')
-	of.write(levels[i])
-	of.close()
-# leaves one bad file at the end
+#!/usr/bin/env python3
+
+import os
+from os import path
+import re
+import sys
+
+file_name = sys.argv[1]
+dir_name = sys.argv[2]
+
+with open(file_name, 'r') as f:
+	s = f.read()
+	p = ';.*\n\n'
+	levels = re.split(p, s)
+
+	os.mkdir(dir_name)
+	for i in range(len(levels)):
+		if levels[i] == '' or levels[i].isspace():
+			continue
+
+		of = open(path.join(dir_name, str(i + 1) + ".txt"), 'w')
+		of.write(levels[i])
+		of.close()
