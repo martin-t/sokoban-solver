c92a4856c730af05dea4e1242b9ebfefe1453baa
Martin Taibr <taibr.martin@gmail.com>
2018-07-20 02:16:09 +0200

    just use one quick benchmark given even youtube in the background causes 10% slowdowns

--- benches/criterion_benches.rs
+++ benches/criterion_benches.rs
@@ -8,26 +8,33 @@ use criterion::{Benchmark, Criterion};
 use sokoban_solver::config::Method;
 use sokoban_solver::{LoadLevel, Solve};
 
+// allowing unused so i can bench just one or few
+// and still notice other warnings if thre are any
+#[allow(unused)]
 fn benech_boxxle_1(c: &mut Criterion) {
     // 3 goals in a row
     bench_level(c, Method::Pushes, "levels/boxxle1/1.txt", 100);
 }
 
+#[allow(unused)]
 fn benech_boxxle_5(c: &mut Criterion) {
     // 4 boxes goal room
     bench_level(c, Method::Pushes, "levels/boxxle1/5.txt", 100);
 }
 
+#[allow(unused)]
 fn benech_boxxle_18(c: &mut Criterion) {
     // 6 boxes - tiny goalroom
     bench_level(c, Method::Pushes, "levels/boxxle1/18.txt", 25);
 }
 
+#[allow(unused)]
 fn benech_boxxle_108(c: &mut Criterion) {
     // 6 boxes in the middle
     bench_level(c, Method::Pushes, "levels/boxxle1/108.txt", 50);
 }
 
+#[allow(unused)]
 fn benech_boxxle_1_moves(c: &mut Criterion) {
     // 3 goals in a row
     bench_level(c, Method::Moves, "levels/boxxle1/1.txt", 100);
@@ -51,9 +58,9 @@ fn bench_level(c: &mut Criterion, method: Method, level_path: &str, samples: usi
 criterion_group!(
     benches,
     benech_boxxle_1,
-    benech_boxxle_5,
-    benech_boxxle_18,
-    benech_boxxle_108,
-    benech_boxxle_1_moves,
+    //benech_boxxle_5,
+    //benech_boxxle_18,
+    //benech_boxxle_108,
+    //benech_boxxle_1_moves,
 );
 criterion_main!(benches);
