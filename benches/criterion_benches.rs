883fd4f0efb8cab1f700b5e2f50aa97c25128b9e
Martin Taibr <taibr.martin@gmail.com>
2018-08-17 13:42:55 +0200

    add 2 benches, adjust running times

--- benches/criterion_benches.rs
+++ benches/criterion_benches.rs
@@ -11,31 +11,43 @@ use sokoban_solver::{LoadLevel, Solve};
 // allowing unused so i can bench just one or a few
 // and still notice other warnings if there are any
 #[allow(unused)]
-fn benech_boxxle_1(c: &mut Criterion) {
+fn benech_pushes_boxxle1_1(c: &mut Criterion) {
     // 3 goals in a row
-    bench_level(c, Method::Pushes, "levels/boxxle1/1.txt", 200);
+    bench_level(c, Method::Pushes, "levels/boxxle1/1.txt", 150);
 }
 
 #[allow(unused)]
-fn benech_boxxle_5(c: &mut Criterion) {
+fn benech_pushes_boxxle1_5(c: &mut Criterion) {
     // 4 boxes goal room
-    bench_level(c, Method::Pushes, "levels/boxxle1/5.txt", 100);
+    bench_level(c, Method::Pushes, "levels/boxxle1/5.txt", 75);
 }
 
 #[allow(unused)]
-fn benech_boxxle_18(c: &mut Criterion) {
+fn benech_pushes_boxxle1_18(c: &mut Criterion) {
     // 6 boxes - tiny goalroom
     bench_level(c, Method::Pushes, "levels/boxxle1/18.txt", 25);
 }
 
 #[allow(unused)]
-fn benech_boxxle_108(c: &mut Criterion) {
+fn benech_pushes_boxxle1_108(c: &mut Criterion) {
     // 6 boxes in the middle
     bench_level(c, Method::Pushes, "levels/boxxle1/108.txt", 50);
 }
 
 #[allow(unused)]
-fn benech_boxxle_1_moves(c: &mut Criterion) {
+fn benech_pushes_boxxle2_3(c: &mut Criterion) {
+    // 5 separate goals
+    bench_level(c, Method::Pushes, "levels/boxxle2/3.txt", 75);
+}
+
+#[allow(unused)]
+fn benech_pushes_boxxle2_4(c: &mut Criterion) {
+    // 13 goals in a checkerboard
+    bench_level(c, Method::Pushes, "levels/boxxle2/4.txt", 25);
+}
+
+#[allow(unused)]
+fn benech_moves_boxxle1_1(c: &mut Criterion) {
     // 3 goals in a row
     bench_level(c, Method::Moves, "levels/boxxle1/1.txt", 100);
 }
@@ -57,10 +69,12 @@ fn bench_level(c: &mut Criterion, method: Method, level_path: &str, samples: usi
 
 criterion_group!(
     benches,
-    benech_boxxle_1,
-    benech_boxxle_5,
-    //benech_boxxle_18,
-    //benech_boxxle_108,
-    //benech_boxxle_1_moves,
+    benech_pushes_boxxle1_1,
+    benech_pushes_boxxle1_5,
+    //benech_pushes_boxxle1_18,
+    //benech_pushes_boxxle1_108,
+    benech_pushes_boxxle2_3,
+    benech_pushes_boxxle2_4,
+    //benech_moves_boxxle1_1,
 );
 criterion_main!(benches);
