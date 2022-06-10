3995fba9efc5df4c10a4d7d75c3b4b66c8546b79
Martin Taibr <taibr.martin@gmail.com>
2022-06-10 08:04:27 +0200

    wip

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,25 @@
-use std::fmt::{self, Display, Formatter};
+use std::{
+    error::Error,
+    fmt::{self, Display, Formatter},
+    str::FromStr,
+};
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Format {
     Custom,
     Xsb,
+}
+
+impl FromStr for Format {
+    type Err = CliError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "custom" => Ok(Self::Custom),
+            "xsb" => Ok(Self::Xsb),
+            _ => Err(CliError),
+        }
+    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -13,6 +29,21 @@ pub enum Method {
     PushesMoves,
     Pushes,
     Any,
+}
+
+impl FromStr for Method {
+    type Err = CliError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "moves-pushes" => Ok(Method::MovesPushes),
+            "moves" => Ok(Method::Moves),
+            "pushes-moves" => Ok(Method::PushesMoves),
+            "pushes" => Ok(Method::Pushes),
+            "any" => Ok(Method::Any),
+            _ => Err(CliError),
+        }
+    }
 }
 
 impl Display for Method {
@@ -26,3 +57,14 @@ impl Display for Method {
         }
     }
 }
+
+#[derive(Debug)]
+pub struct CliError;
+
+impl Display for CliError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "failed to parse")
+    }
+}
+
+impl Error for CliError {}
--- src/main.rs
+++ src/main.rs
@@ -16,108 +16,119 @@
 use std::fs;
 use std::process;
 
-use clap::{crate_authors, crate_version};
-use clap::{App, Arg, ArgGroup};
+use clap::Parser;
 
 use sokoban_solver::config::{Format, Method};
 use sokoban_solver::{LoadLevel, Solve};
 
+#[derive(Parser)]
+struct Args {
+    format: Format,
+
+    method: Method,
+
+    #[clap(long, short)]
+    verbose: bool,
+}
+
 fn main() {
-    // if anybody thinks this is overkill, i made a typo twice already
-    const CUSTOM: &str = "custom";
-    const XSB: &str = "xsb";
-    const MOVES_PUSHES: &str = "moves-pushes";
-    const MOVES: &str = "moves";
-    const PUSHES_MOVES: &str = "pushes-moves";
-    const PUSHES: &str = "pushes";
-    const ANY: &str = "any";
-    const LEVEL_FILE: &str = "level-file";
-    const VERBOSE: &str = "verbose";
+    // // if anybody thinks this is overkill, i made a typo twice already
+    // const CUSTOM: &str = "custom";
+    // const XSB: &str = "xsb";
+    // const MOVES_PUSHES: &str = "moves-pushes";
+    // const MOVES: &str = "moves";
+    // const PUSHES_MOVES: &str = "pushes-moves";
+    // const PUSHES: &str = "pushes";
+    // const ANY: &str = "any";
+    // const LEVEL_FILE: &str = "level-file";
+    // const VERBOSE: &str = "verbose";
 
-    let app = App::new("sokoban-solver")
-        .author(crate_authors!())
-        .version(crate_version!())
-        .arg(
-            Arg::with_name(CUSTOM)
-                .short("c")
-                .long(CUSTOM)
-                .help("Output in the custom format"),
-        )
-        .arg(
-            Arg::with_name(XSB)
-                .short("x")
-                .long(XSB)
-                .help("Output in the XSB format (default)"),
-        )
-        .group(ArgGroup::with_name("format").args(&[CUSTOM, XSB]))
-        .arg(
-            Arg::with_name(MOVES_PUSHES)
-                .short("M")
-                .long(MOVES_PUSHES)
-                .help("Search for a move-optimal solution with minimal pushes"),
-        )
-        .arg(
-            Arg::with_name(MOVES)
-                .short("m")
-                .long(MOVES)
-                .help("Search for a move-optimal solution"),
-        )
-        .arg(
-            Arg::with_name(PUSHES_MOVES)
-                .short("P")
-                .long(PUSHES_MOVES)
-                .help("Search for a push-optimal solution with minimal moves"),
-        )
-        .arg(
-            Arg::with_name(PUSHES)
-                .short("p")
-                .long(PUSHES)
-                .help("Search for a push-optimal solution"),
-        )
-        .arg(
-            Arg::with_name(ANY)
-                .short("a")
-                .long(ANY)
-                .help("Search for any solution (default, currently push optimal)"),
-        )
-        .group(ArgGroup::with_name("method").args(&[
-            MOVES_PUSHES,
-            MOVES,
-            PUSHES_MOVES,
-            PUSHES,
-            ANY,
-        ]))
-        .arg(Arg::with_name(LEVEL_FILE).required(true).multiple(true));
+    // let app = Command::new("sokoban-solver")
+    //     .author(crate_authors!())
+    //     .version(crate_version!())
+    //     .arg(
+    //         Arg::with_name(CUSTOM)
+    //             .short("c")
+    //             .long(CUSTOM)
+    //             .help("Output in the custom format"),
+    //     )
+    //     .arg(
+    //         Arg::with_name(XSB)
+    //             .short("x")
+    //             .long(XSB)
+    //             .help("Output in the XSB format (default)"),
+    //     )
+    //     .group(ArgGroup::with_name("format").args(&[CUSTOM, XSB]))
+    //     .arg(
+    //         Arg::with_name(MOVES_PUSHES)
+    //             .short("M")
+    //             .long(MOVES_PUSHES)
+    //             .help("Search for a move-optimal solution with minimal pushes"),
+    //     )
+    //     .arg(
+    //         Arg::with_name(MOVES)
+    //             .short("m")
+    //             .long(MOVES)
+    //             .help("Search for a move-optimal solution"),
+    //     )
+    //     .arg(
+    //         Arg::with_name(PUSHES_MOVES)
+    //             .short("P")
+    //             .long(PUSHES_MOVES)
+    //             .help("Search for a push-optimal solution with minimal moves"),
+    //     )
+    //     .arg(
+    //         Arg::with_name(PUSHES)
+    //             .short("p")
+    //             .long(PUSHES)
+    //             .help("Search for a push-optimal solution"),
+    //     )
+    //     .arg(
+    //         Arg::with_name(ANY)
+    //             .short("a")
+    //             .long(ANY)
+    //             .help("Search for any solution (default, currently push optimal)"),
+    //     )
+    //     .group(ArgGroup::with_name("method").args(&[
+    //         MOVES_PUSHES,
+    //         MOVES,
+    //         PUSHES_MOVES,
+    //         PUSHES,
+    //         ANY,
+    //     ]))
+    //     .arg(Arg::with_name(LEVEL_FILE).required(true).multiple(true));
 
-    #[cfg(debug_assertions)]
-    let app = app.arg(
-        Arg::with_name(VERBOSE)
-            .short("v")
-            .long(VERBOSE)
-            .help("Print all log levels (only available in debug builds)"),
-    );
+    // #[cfg(debug_assertions)]
+    // let app = app.arg(
+    //     Arg::with_name(VERBOSE)
+    //         .short("v")
+    //         .long(VERBOSE)
+    //         .help("Print all log levels (only available in debug builds)"),
+    // );
 
-    let matches = app.get_matches();
+    // let matches = app.get_matches();
 
-    let format = if matches.is_present(CUSTOM) {
-        Format::Custom
-    } else {
-        Format::Xsb
-    };
+    // let format = if matches.is_present(CUSTOM) {
+    //     Format::Custom
+    // } else {
+    //     Format::Xsb
+    // };
 
-    let method = if matches.is_present(MOVES_PUSHES) {
-        Method::MovesPushes
-    } else if matches.is_present(MOVES) {
-        Method::Moves
-    } else if matches.is_present(PUSHES_MOVES) {
-        Method::PushesMoves
-    } else if matches.is_present(PUSHES) {
-        Method::Pushes
-    } else {
-        Method::Any
-    };
+    // let method = if matches.is_present(MOVES_PUSHES) {
+    //     Method::MovesPushes
+    // } else if matches.is_present(MOVES) {
+    //     Method::Moves
+    // } else if matches.is_present(PUSHES_MOVES) {
+    //     Method::PushesMoves
+    // } else if matches.is_present(PUSHES) {
+    //     Method::Pushes
+    // } else {
+    //     Method::Any
+    // };
 
-    let verbose = matches.is_present(VERBOSE);
+    // let verbose = matches.is_present(VERBOSE);
+
+    let args = Args::parse();
 
     let log_level = if verbose {
         log::LevelFilter::Trace
